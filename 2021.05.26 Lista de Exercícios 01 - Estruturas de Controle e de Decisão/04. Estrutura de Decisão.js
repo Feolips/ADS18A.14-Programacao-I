dc834f7591f1ae69bde9824e039777a9a8bd836b
Felipe Souza Vieira <feolips@outlook.com>
2021-06-16 18:18:00 -0300

    update README.md

--- 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/04. Estrutura de Decisão.js
+++ 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/04. Estrutura de Decisão.js
@@ -1,4 +1,8 @@
 // 				ESTRUTURA DE DECISÃO
 // 04. Ler dois valores, calcular a diferença entre o maior e o menor, e exibir tudo:
 
-
+let A = 2;
+let B = 4;
+function diferenca{
+	if A > B
+}
---
+++ 2021.06.09 Lista de Exercício 03 - Arrays/01. Arrays.js
@@ -0,0 +1,5 @@
+// 				ARRAYS
+// 01. Ler um array B de 10 elementos que podem ser repetidos.
+//     Os elementos únicos de B devem ser gravados no array C.
+
+
