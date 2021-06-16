50e3638ac0d963b879134e406cda847e4b92eecc
Felipe Souza Vieira <feolips@outlook.com>
2021-06-16 18:36:01 -0300

    começando arrays

--- 2021.06.09 Lista de Exercício 03 - Arrays/01. Arrays.js
+++ 2021.06.09 Lista de Exercício 03 - Arrays/01. Arrays.js
@@ -2,4 +2,11 @@
 // 01. Ler um array B de 10 elementos que podem ser repetidos.
 //     Os elementos únicos de B devem ser gravados no array C.
 
-
+// Array:
+const B = [5,2,5,1,2,6,7,9,6,0];
+// Laço for:
+for (i = 0; i < B.lenght; i++){
+	// Condicional:
+	if () && (){
+	}
+}
