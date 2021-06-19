d5171e297f46815df922bcecc290b853dc83e74b
Felipe Souza Vieira <feolips@outlook.com>
2021-06-19 18:21:24 -0300

    Questão 12 Resolvida

--- 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/12. Estrutura de Decisão.js
+++ 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/12. Estrutura de Decisão.js
@@ -1,7 +1,6 @@
 // 				ESTRUTURA DE DECISÃO
 // 12.receba três notas e calcule a média ponderada com pesos 2,5, 3,5 e 4.
 // Exibir as três notas e a média calculada.
-// Média = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3)
 
 // Entrada de dados
 input = require("prompt-sync")();
