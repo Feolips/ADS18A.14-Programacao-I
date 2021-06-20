d9f2f6f9a27738f6af05ae4aee631c346810204f
Felipe Souza Vieira <feolips@outlook.com>
2021-06-19 23:49:17 -0300

    Questão 13 Resolvida

--- 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/13. Estrutura de Decisão.js
+++ 2021.05.26 Lista de Exercícios 01 - Estruturas de Controle e de Decisão/13. Estrutura de Decisão.js
@@ -14,7 +14,7 @@ for (i = 1; i < 4; i++){
 let ladoOrdenado = [];
 if (lado[1] > lado[2] && lado[1] > lado[3]){
 	ladoOrdenado[1] = lado[1];
-	if (lado[2] > lado[3]){
+	if (lado[2] >= lado[3]){
 		ladoOrdenado[2] = lado[2];
 		ladoOrdenado[3] = lado[3];
 	} else {
@@ -23,7 +23,7 @@ if (lado[1] > lado[2] && lado[1] > lado[3]){
 	  }
 }
 
-if (lado[2] > lado[1] && lado[2] > lado[3]){
+else if (lado[2] > lado[1] && lado[2] > lado[3]){
 	ladoOrdenado[1] = lado[2];
 	if (lado[1] > lado[3]){
 		ladoOrdenado[2] = lado[1];
@@ -34,7 +34,7 @@ if (lado[2] > lado[1] && lado[2] > lado[3]){
 	  }
 }
 
-if (lado[3] > lado[1] && lado[3] > lado[2]){
+else if (lado[3] > lado[1] && lado[3] > lado[2]){
 	ladoOrdenado[1] = lado[3];
 	if (lado[1] > lado[2]){
 		ladoOrdenado[2] = lado[1];
@@ -44,10 +44,16 @@ if (lado[3] > lado[1] && lado[3] > lado[2]){
 		ladoOrdenado[3] = lado[1];
 	  }
 }
+// Caso todos sejam iguais:
+else{
+	for (i = 1; i < 4; i++){
+		ladoOrdenado[i] = lado[i];
+	}
+}
 
 // Testando se a soma dos lados menores superam o maior
 // e retornando o resultado:
 
-if (ladoOrdenado[1] > (ladoOrdenado[2] + ladoOrdenado[3])){
+if (ladoOrdenado[1] < (ladoOrdenado[2] + ladoOrdenado[3])){
 	console.log("Os vetores " + lado[1] + ", " + lado[2] + " e " + lado[3] + " formam um triângulo.");
 } else {console.log("Os vetores " + lado[1] + ", " + lado[2] + " e " + lado[3] + " não formam um triângulo.");}
