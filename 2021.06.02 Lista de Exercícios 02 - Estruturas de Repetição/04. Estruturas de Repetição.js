63489fe56fad3c63786792b8ab440eeac65eb795
Felipe Souza Vieira <feolips@outlook.com>
2021-06-25 18:30:57 -0300

    Questão 04 Resolvida

--- 2021.06.02 Lista de Exercícios 02 - Estruturas de Repetição/04. Estruturas de Repetição.js
+++ 2021.06.02 Lista de Exercícios 02 - Estruturas de Repetição/04. Estruturas de Repetição.js
@@ -1,20 +1,24 @@
 
-//					ESTRUTURA DE REPETIÇÃO
+//							ESTRUTURA DE REPETIÇÃO
 // 04. Dadas as notas obtidas por cada aluno de uma disciplina, escrever um pro-
 //     grama para ler essas notas. O programa deve calcular e exibir a média a-
 //     ritmética das notas dos alunos. Considerar o flag como sendo a nota 99.
 
 // Entrada de dados
 input = require("prompt-sync")();
-let ciclo, nota = 0;
+let ciclo = 0;
+let somatorio = 0;
+let nota = 0;
+
 while (nota != 99){
-	if (nota === 99) { break; }
+	if (nota === 99); // { break; }
 	ciclo++
-	nota += parseInt(input("Atribua nova nota ao aluno (ou 99 para sair): "));
+	nota = parseInt(input("Atribua nova nota ao aluno (ou 99 para sair): "));
+	somatorio += nota;
 }
-nota = nota - 99; // Isso resolve o excesso de nota :D
-media = nota/ciclo;
+somatorio = somatorio - 99; // Isso resolve o excesso de nota :D
+media = somatorio/ciclo;
 // Retorno
-console.log("O aluno somou um total de " + nota + " atingindo média aritmética de " + media + ".");
+console.log("O aluno somou um total de " + somatorio + " atingindo média aritmética de " + media + ".");
 
 
